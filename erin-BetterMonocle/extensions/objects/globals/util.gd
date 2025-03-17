bf1e9499774d498b8706966da09226ecaf2d1c1c
Erin Miller <erinm1018@gmail.com>
2025-03-17 18:21:53 -0500

    v1.0.2

--- erin-BetterMonocle/extensions/objects/globals/util.gd
+++
@@ -1,8 +0,0 @@
-extends "res://objects/globals/util.gd"
-
-func get_player() -> Player:
-	var player = super()
-	if not is_instance_valid(player):
-		return null
-	player.see_descriptions = true
-	return player
